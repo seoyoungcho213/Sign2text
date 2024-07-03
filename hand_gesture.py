9aac186a5e64fbbe51cb32ad0297c9771b44b728
anushasarraf <anusha.sarraf2001@gmail.com>
2024-07-02 20:17:30 -0700

    Update hand_gesture.py

--- hand_gesture.py
+++ hand_gesture.py
@@ -20,5 +20,11 @@ while capture.isOpened():
     cv2.rectangle(frame, (100, 100), (300, 300), (0, 255, 0), 0)
     crop_image = frame[100:300, 100:300]
 
+    # blurring image to smooth out edges
+    blur = cv2.GaussianBlur(crop_image, (3, 3), 0)
+
+    # changing color from BGR(Blue, Green, Red) to HSV(Hue, Saturation, Value)
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+
 
 
